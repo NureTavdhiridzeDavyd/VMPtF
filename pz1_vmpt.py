b6b04b53a92b0208633a3814814168dbf8fc7092
NureTavdhiridzeDavyd <davyd.tavdhiridze@nure.ua>
2026-03-19 08:34:22 +0100

    Add files via upload

---
+++ pz1_vmpt.py
@@ -0,0 +1,29 @@
+# ctrl k ctrl c ctrl k ctrl u 
+for i in range(1, 11):
+    print(i)
+
+# a = float(input("numero uno: "))
+# b = float(input("numero dos: "))
+# c = float(input("numero tres: "))
+# average = (a + b + c) / 3
+# print(f"average: {average}")
+
+# class Calculator:
+#     def add(self, a, b):
+#         return a + b
+#     def subtract(self, a, b):
+#         return a - b
+#     def multiply(self, a, b):
+#         return a * b
+#     def divide(self, a, b):
+#         if b != 0:
+#             return a / b
+#         return "ділити на нуль не можна"
+
+# calc = Calculator()
+# x = 10
+# y = 5
+# print(f"+: {calc.add(x, y)}")
+# print(f"-: {calc.subtract(x, y)}")
+# print(f"*: {calc.multiply(x, y)}")
+# print(f": : {calc.divide(x, y)}")
