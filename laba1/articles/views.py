2b9126770f9f0b6419c831dcc8f950550153b731
NureTavdhiridzeDavyd <davyd.tavdhiridze@nure.ua>
2026-04-25 09:27:43 +0200

    Add files via upload

---
+++ laba1/articles/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Article
+
+def article_list(request):
+    articles = Article.objects.all()
+    context = {'articles': articles}
+    return render(request, 'articles/article_list.html', context)
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    context = {'article': article}
+    return render(request, 'articles/article_detail.html', context)
